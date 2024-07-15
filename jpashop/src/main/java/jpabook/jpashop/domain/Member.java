3fda287d32718ef2c1cee9cf90e6d7938d466401
jinhyeon <tiemo0708@naver.com>
2024-07-15 19:10:18 +0900

    feat: 엔티티 클래스 개발2

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -18,7 +18,7 @@ public class Member {
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy="order")
+    @OneToMany(mappedBy="member")
     private List<Order> orders = new ArrayList<>();
 
 }
