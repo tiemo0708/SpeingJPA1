d26e29302baf7b986d5c8885a94adf473fab2711
jinhyeon <tiemo0708@naver.com>
2024-07-25 14:11:13 +0900

    feat: 주문, 쭈문상품 엔티티 개발(비즈니스, 조회 로직)

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -32,7 +32,7 @@ public class Order {
 
     private OrderStatus status; //주문 상태 [ORDER, CANCEL]
 
-    //==연관관계 편의 메서드 ==
+    //==연관관계 편의 메서드 ==//
     public void setMember(Member member){
         this.member = member;
         member.getOrders().add(this);
@@ -41,5 +41,43 @@ public class Order {
     public void addOrderItem(OrderItem orderItem){
         orderItems.add(orderItem);
         orderItem.setOrder(this);
+    }
+    //==생성 메서드 ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItemm : orderItems) {
+            order.addOrderItem(orderItemm);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직 ==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DelivaryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능 합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem: orderItems){
+            orderItem.cancel();
+        }
+    }
+    //==조회 로직 ==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public  int getTotalPrice(){
+//     int totalPrice = 0;
+//     for(OrderItem orderItem: orderItems){
+//         totalPrice += orderItem.getTotalPrice();
+//     }
+//     return totalPrice;
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
     }
 }
