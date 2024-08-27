6ac80c13815f11ae9b8928d994a5f1cd85f32bb0
jinhyeon <tiemo0708@naver.com>
2024-08-27 18:06:21 +0900

    feat: 간단한 주문조회 V1 - 엔티티 직접 노출

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;  // 변경된 부분
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +12,11 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate6Module hibernate6Module() {  // 변경된 부분
+		Hibernate6Module hibernate6Module = new Hibernate6Module();
+		// 강제 지연 로딩 설정
+//		hibernate6Module.configure(Hibernate6Module.Feature.FORCE_LAZY_LOADING, true);  // 엔티티 외부 노출
+		return hibernate6Module;
+	}
 }
